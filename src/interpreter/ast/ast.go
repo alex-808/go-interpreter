ca9b8efb98d52f2e5331ac0266ecd57fdb7975e1
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-11 14:08:14 -0700

    Fix bug due to no 'ReturnValue' field on ReturnStatement type and add ExpressionStatement node type to AST

--- src/interpreter/ast/ast.go
+++ src/interpreter/ast/ast.go
@@ -1,12 +1,17 @@
 package ast
 
-import "github.com/alex-davis-808/go-interpreter/src/interpreter/token"
+import (
+	"bytes"
+
+	"github.com/alex-davis-808/go-interpreter/src/interpreter/token"
+)
 
 // every node in the AST will have to implement this Node interface
 type Node interface {
 	// nodes must provide a TokenLiteral() method which returns the literal val of the token it's associated with
 	// will only used for debugging and testing
 	TokenLiteral() string
+	String() string //used for debugging and testing
 }
 
 // Statement and Expression interfaces define types of Nodes
@@ -34,6 +39,17 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	//returns buffer as string
+	return out.String()
+}
+
 type LetStatement struct {
 	// it will have a 'LET' token type and it's literal "let"
 	Token token.Token
@@ -47,6 +63,22 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type Identifier struct {
 	// will have an 'IDENT' token
 	Token token.Token
@@ -59,11 +91,41 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+func (i *Identifier) String() string { return i.Value }
+
 type ReturnStatement struct {
 	// a 'RETURN' token
-	Token token.Token
-	Value Expression
+	Token       token.Token
+	ReturnValue Expression
 }
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
