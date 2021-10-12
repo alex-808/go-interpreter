cf035e93a24d57709aa751b4f4079f0499165f5a
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-12 09:45:09 -0700

    Add parsing of integer literal expression statements.

--- src/interpreter/parser/parser.go
+++ src/interpreter/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/alex-davis-808/go-interpreter/src/interpreter/ast"
 	"github.com/alex-davis-808/go-interpreter/src/interpreter/lexer"
@@ -39,7 +40,9 @@ func New(l *lexer.Lexer) *Parser {
 
 	// make the maps specified on the type
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	// register fns for token types
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Read two tokens so both curToken and peekToken are set
 	p.nextToken()
@@ -152,6 +155,21 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
 
 // Just checks if current token is of the expected type
